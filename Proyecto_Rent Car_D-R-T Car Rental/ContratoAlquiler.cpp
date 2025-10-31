1107fd46d343c858caa69914c093ae2f962b2b11
LianSVM19 <lianvillalta@gmail.com>
2025-10-31 14:32:12 -0600

    se realizo ContratoAlquiler

--- Proyecto_Rent Car_D-R-T Car Rental/ContratoAlquiler.cpp
+++ Proyecto_Rent Car_D-R-T Car Rental/ContratoAlquiler.cpp
@@ -1,1 +1,63 @@
 #include "ContratoAlquiler.h"
+#include "SolicitudAlquiler.h"
+
+using namespace std;
+
+// ----------------------
+// Constructor 
+// ----------------------
+ContratoAlquiler::ContratoAlquiler(string cod, double costo, string est, SolicitudAlquiler* sol) {
+    codigoContrato = cod;
+    costoTotal = costo;
+    estado = est;
+    solicitud = sol;
+}
+
+// ----------------------
+// Destructor 
+// ----------------------
+ContratoAlquiler::~ContratoAlquiler() {
+    solicitud = NULL;
+}
+
+// ----------------------
+// Getters 
+// ----------------------
+string ContratoAlquiler::getCodigoContrato() { return codigoContrato; }
+double ContratoAlquiler::getCostoTotal() { return costoTotal; }
+string ContratoAlquiler::getEstado() { return estado; }
+SolicitudAlquiler* ContratoAlquiler::getSolicitud() { return solicitud; }
+
+// ----------------------
+// Setters 
+// ----------------------
+void ContratoAlquiler::setCodigoContrato(string cod) { codigoContrato = cod; }
+void ContratoAlquiler::setCostoTotal(double costo) { costoTotal = costo; }
+void ContratoAlquiler::setEstado(string est) { estado = est; }
+void ContratoAlquiler::setSolicitud(SolicitudAlquiler* sol) { solicitud = sol; }
+
+// ----------------------
+// Función "to string"
+// ----------------------
+string ContratoAlquiler::toString() {
+    stringstream s;
+
+    s << "\t\t*************************************************" << endl;
+    s << "\t\t--- DOCUMENTO DE CONTRATO DE ALQUILER ---" << endl;
+    s << "\t\tCódigo de Contrato: " << codigoContrato << endl;
+    s << "\t\tCosto Total Acordado: $" << costoTotal << endl;
+    s << "\t\tEstado del Contrato: " << estado << endl;
+    s << "\t\t-------------------------------------------------" << endl;
+
+    if (solicitud != NULL) {
+        s << "\t\tSOLICITUD DE ORIGEN (Código: " << solicitud->getCodigoSoli() << "):" << endl;
+        s << solicitud->toString();
+    }
+    else {
+        s << "\t\tSOLICITUD: [ERROR: Objeto NULL o no asignado]" << endl;
+    }
+
+    s << "\t\t*************************************************" << endl;
+
+    return s.str();
+}
--- Proyecto_Rent Car_D-R-T Car Rental/ContratoAlquiler.h
+++ Proyecto_Rent Car_D-R-T Car Rental/ContratoAlquiler.h
@@ -1,6 +1,40 @@
-#pragma once
-class ContratoAlquiler
-	// Se genera al aprobar una solicitud
-{
+#ifndef CONTRATOALQUILER_H
+#define CONTRATOALQUILER_H
+
+#include "SolicitudAlquiler.h" 
+
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class ContratoAlquiler {
+private:
+    string codigoContrato;
+    double costoTotal;
+    string estado;
+
+    SolicitudAlquiler* solicitud;
+
+public:
+    // Constructor: 
+    ContratoAlquiler(string, double, string, SolicitudAlquiler*);
+
+    // Destructor
+    ~ContratoAlquiler();
+
+    // Getters y Setters
+    string getCodigoContrato();
+    double getCostoTotal();
+    string getEstado();
+    SolicitudAlquiler* getSolicitud();
+
+    void setCodigoContrato(string);
+    void setCostoTotal(double);
+    void setEstado(string);
+    void setSolicitud(SolicitudAlquiler*);
+
+    string toString();
 };
 
+#endif
