3d290a1ef673ab5d7446da0305b436d0148c2f94
LianSVM19 <lianvillalta@gmail.com>
2025-11-02 23:22:30 -0600

    ala puta

--- Proyecto_Rent Car_D-R-T Car Rental/D-R-T_Car_Rental.cpp
+++ Proyecto_Rent Car_D-R-T Car Rental/D-R-T_Car_Rental.cpp
@@ -3,28 +3,11 @@
 #include "Utilidades.h"
 using namespace std;
 int main() {
-    cout << "EEEEEEaaaaaaa" << endl;
-    cout << "madre mia willy" << endl;
-    cout << "peroquehacesaquicompa"<<char(164)<<"ero" << endl;
-    // 1. Crear una instancia de la clase Utilidades
-    // Esto es el único punto de conexión con la clase.
-    
     Utilidades miSistema;
 
-    // 2. Llamar al método principal que inicia la lógica del menú
     miSistema.ejecutarSistema();
 
     miSistema.liberarMemoria();
-    cout << "peroquehacesaquicompa" << char(164) << "ero" << endl;
-    //prueba de clase vehiculo.
-    
-    
-    /*char x;
-    Vehiculo* n = new Vehiculo(); //este constructor esta malo pero aja.
-    cout << "Categorias: " << endl;
-    cout << "A, B, C, D" << endl;
-    cin >> x;
-    n->setCategoria(x);
-    delete n;*/
+
     return 0;
 }
